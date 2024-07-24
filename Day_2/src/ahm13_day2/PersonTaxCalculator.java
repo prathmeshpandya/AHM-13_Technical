d0f6e2d93a3e7f394d2cd0f23de0f6219323da9f
Prathmesh <Prathmesh@Prathmesh-06>
2024-07-24 18:33:43 +0530

    Day 2

---
+++ Day_2/src/ahm13_day2/PersonTaxCalculator.java
@@ -0,0 +1,27 @@
+package ahm13_day2;
+
+import java.util.Scanner;
+
+public class PersonTaxCalculator {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your name");
+		String name=sc.next();
+		
+		System.out.println("Enter your age");
+		int age=sc.nextInt();
+		
+		System.out.println("Enter your gender");
+		String gender=sc.next();
+		
+		System.out.println("Enter your income");
+		int income=sc.nextInt();
+		
+		Person p1=new Person(name,age,gender,income);
+		TaxCalculator tax=new TaxCalculator();
+		tax.calTax(p1);
+		
+		System.out.println(p1);
+	}
+}
